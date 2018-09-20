1141668f165414bd7eb5a572d7bb565e98da28ef
agricolamz <agricolamz@gmail.com>
2018-09-20 17:09:45 +0300

    first commit

---
+++ RSL-phonology-preprint.md
@@ -0,0 +1,23 @@
+---
+lang: en-GB
+---
+
+* detailed descriptions of individual sign language phonological systems are rare (van der Kooij; Plaskovitskaya + some more)
+* more typical of phonological research on SLs are solutions to narrowly formulated problems (EXAMPLES)
+* SLs and their properties
+    * iconicity
+    * segments (REF) aren't easily identifiable 
+    * *ergo* determining the phonological primitives and their composition is far from obvious
+* aims
+    * showcase phonological diversity of SL lexicon based on the phonological description of the RSL phonological system in its entirety
+* claims
+    * using even a small amount of data and a crude phonological description it is possible to detect some large phonological patterns in SL lexicon
+* materials and method
+    * phonological description of the Belarusian dialect of RSL [@Plaskovitskaya:2018] 
+    * and her annotation of ca. 400 monosyllabic (REF!) verbal forms from the SpreadTheSign database
+    * MCA analysis for all entries
+* we discovered 3 clusters corresponding to 
+    * two-hand signs 
+    * cluster 2
+    * cluster 3
+    
---
+++ extracting_data.R
@@ -0,0 +1,57 @@
+setwd("/home/agricolamz/work/articles/2018_Tosha_preprint_SL_annotation_verbs")
+library(tidyverse)
+library(xml2)
+
+# get the column names from one file
+df1 <- read_xml("Term_paper/dataAnatation/10030.eaf")
+sapply(3:23, function(x){
+  xml_attrs(xml_child(df1, x))[["TIER_ID"]]
+}) -> columns
+
+# create an empty df
+final <- matrix(nrow = 401, ncol = 22)
+
+# create a list of files for analysis
+files <- str_extract(list.files("Term_paper/dataAnatation/"), ".*\\.eaf")
+
+sapply(seq_along(files), function(i){
+  df <- read_xml(paste0("Term_paper/dataAnatation/", files[i]))
+  
+  sapply(3:23, function(x){
+    if(str_detect(as.character(xml_child(df, x)), "ANNOTATION_VALUE")){
+      str_sub(str_extract(as.character(
+        xml_child(xml_child(xml_child(df, x)))), 
+        "ANNOTATION_VALUE>.*</ANNOTATION_VALUE"), 18, -19)
+    } else{
+        ""
+      }
+  }) -> values
+  values <- c(values, xml_attrs(xml_child(xml_child(df, 1), 1))[["RELATIVE_MEDIA_URL"]])
+  
+  final[i,] <<- values
+
+  })
+
+final2 <- final
+
+as.tibble(final) ->
+  final
+
+colnames(final) <- c(columns, "url")
+ 
+final %>% 
+  mutate_all(.funs = as.character) %>% 
+  mutate(url = paste0(
+           "https://github.com/ToszaPlaskovickaja/Term_paper/blob/master/data",
+           str_replace_all(url, "\\.\\.", ""),
+           "?raw=true"),
+         labeled_url = paste0(
+           "<a href='",
+           url,
+           "' target='_blank'>",
+           Sign,
+           "</a>"
+         )) ->
+  final
+
+write_csv(final, "Tosha_annotation.csv", na = "")
